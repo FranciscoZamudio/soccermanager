607c33b94e9e150bc463df60fe7cb29060f14491
francisco <fj_darkthunder@hotmail.com>
2019-01-13 14:43:55 -0600

    continue torunament view

---
+++ controllers/continueTournamentController.js
@@ -0,0 +1,38 @@
+//we inject the controller with a model
+moduleApp.controller("continueTournamentController", function($scope, ticketsService, $http){
+
+
+
+  $scope.postData = function(info){
+    $http({
+          url : "./models/ticketsModel.php",
+          method: 'POST',
+          data: info,
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function mySuccess(response) {
+          console.log("the ticket has been registered");
+      }, function myError(response) {
+          console.log("there was an error");
+      });
+  };
+
+
+
+$scope.master = {};
+
+$scope.createTournament = function(tournament) {
+  //var info = 'action=addTicket' + '&ticketInfo=' + JSON.stringify(tournament);
+  //$scope.postData(info);
+  console.log($scope.players);
+};
+
+
+$scope.playersInput = function(numberPlayers) {
+  $scope.players = [];
+  for (i = 0; i < numberPlayers; i++) {
+    $scope.players.push({name:''});
+  }
+};
+
+
+});
